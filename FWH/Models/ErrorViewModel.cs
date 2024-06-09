400ab34265549f5a329b53e0bb30f1d8a59b6ff4
MuqadsaNawaz <muqadsabaig20@gmail.com>
2024-06-09 09:40:07 -0700

    Add project files.

---
+++ FWH/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FWH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
